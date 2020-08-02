1114b376df96899f88563db908670308bbdbec1b
Nena <nehnakkottakkal44@gmail.com>
2020-08-02 12:43:31 +0530

    test naflu commiit

--- tt.py
+++ tt.py
@@ -62,8 +62,109 @@ timetable={'monday':
              'Rno':'124'
              }
             }
+           ],
+            'wednesday':[
+                {'h1':
+                 {'sub':'maths',
+                 'Tchr':'lf',
+                 'Rno':'129'
+                 }
+                },
+                {'h2':
+                 {'sub':'ds',
+                 'Tchr':'sd',
+                 'Rno':'121'
+                 }
+                },
+                {'h3':
+                 {'sub':'py',
+                 'Tchr':'ar',
+                 'Rno':'122'
+                 }
+                },
+                {'h4':
+             {'sub':'st',
+             'Tchr':'nz',
+             'Rno':'123'
+             }
+            },
+                {'h5':
+             {'sub':'se',
+             'Tchr':'al',
+             'Rno':'124'
+             }
+            }
+           ],
+            'thursday':[
+                {'h1':
+                 {'sub':'maths',
+                 'Tchr':'lf',
+                 'Rno':'129'
+                 }
+                },
+                {'h2':
+                 {'sub':'ds',
+                 'Tchr':'sd',
+                 'Rno':'121'
+                 }
+                },
+                {'h3':
+                 {'sub':'py',
+                 'Tchr':'ar',
+                 'Rno':'122'
+                 }
+                },
+                {'h4':
+             {'sub':'st',
+             'Tchr':'nz',
+             'Rno':'123'
+             }
+            },
+                {'h5':
+             {'sub':'se',
+             'Tchr':'al',
+             'Rno':'124'
+             }
+            }
+           ],
+            'friday':[
+                {'h1':
+                 {'sub':'maths',
+                 'Tchr':'lf',
+                 'Rno':'129'
+                 }
+                },
+                {'h2':
+                 {'sub':'ds',
+                 'Tchr':'sd',
+                 'Rno':'121'
+                 }
+                },
+                {'h3':
+                 {'sub':'py',
+                 'Tchr':'ar',
+                 'Rno':'122'
+                 }
+                },
+                {'h4':
+             {'sub':'st',
+             'Tchr':'nz',
+             'Rno':'123'
+             }
+            },
+                {'h5':
+             {'sub':'se',
+             'Tchr':'al',
+             'Rno':'124'
+             }
+            }
            ]
            }
-print(timetable['monday'][1]['h2']['sub'])
+
+print(timetable['monday'][0]['h1']['sub'])
+print(timetable['tuesday'][1]['h2']['sub'])
+print(timetable['wednesday'][2]['h3']['sub'])
+print(timetable['thursday'][3]['h4']['sub'])
+print(timetable['friday'][4]['h5']['sub'])
 
 
